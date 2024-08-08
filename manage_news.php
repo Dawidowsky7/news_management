6c1045bda645dbf940f7693ebda1db9f66118393
Dawidowsky7 <fanciak@wp.pl>
2024-08-08 21:10:01 +0200

    footer - deleted

--- manage_news.php
+++ manage_news.php
@@ -240,8 +240,6 @@ $newsList = getNews($sort, $filter);
         </div>
     </div>
 
-    <?php include 'templates/footer.php'; ?>
-
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.jquery.min.js"></script>
